872118bc058e0b88fe37d03d030091707739959c
chen411 <chen15735253825@qq.com>
2020-01-06 19:56:01 +0800

    黑

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,12 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
+  ,
+  {
+    path: '/detail',
+    name: 'detail',
+    component: () => import(/* webpackChunkName: "detail" */ '../views/Detail.vue')
+  }
 ]
 
 const router = new VueRouter({
